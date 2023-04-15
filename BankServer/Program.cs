58511300b50396da5b44423bf041134fd92e9bd0
Yan Khonski <yankhonskiy@gmail.com>
2023-04-15 21:51:52 +0200

    Copy classes

---
+++ BankServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Silo.");
+await Host.CreateDefaultBuilder(args)
+    .UseOrleans(siloBuilder =>
+    {
+        siloBuilder
+            .UseLocalhostClustering()
+            .AddMemoryGrainStorageAsDefault()
+            .UseTransactions();
+    })
+    .RunConsoleAsync();
